c1bfe6c034a45d70a5d304f5b7b0d8381a41985a
librautoo <61204127+librautoo@users.noreply.github.com>
2023-12-19 16:20:31 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
-document.querySelectorAll(".post").forEach(post => {
-	const postId = post.id;
-	const ratings = post.querySelectorAll(".post-rating");
-	const likeRating = ratings[0];
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll(".post").forEach(post => {
+        const postId = post.id;
+        const ratings = post.querySelectorAll(".post-rating");
 
-	ratings.forEach(rating => {
-		const button = rating.querySelector(".post-rating-button");
-		const count = rating.querySelector(".post-rating-count");
+        ratings.forEach(rating => {
+            const button = rating.querySelector(".post-rating-button");
+            const count = rating.querySelector(".post-rating-count");
 
-		button.addEventListener("click", async () => {
-			if (rating.classList.contains("post-rating-selected")) {
-				return;
-			}
+            button.addEventListener("click", async () => {
+                if (button.textContent === 'thumb_up' || button.textContent === 'thumb_down') {
+                    animateButton(button);
+                }
 
-			count.textContent = Number(count.textContent) + 1;
+                if (rating.classList.contains("post-rating-selected")) {
+                    return;
+                }
 
-			ratings.forEach(rating => {
-				if (rating.classList.contains("post-rating-selected")) {
-					const count = rating.querySelector(".post-rating-count");
+                incrementCounter(count);
 
-					count.textContent = Math.max(0, Number(count.textContent) - 1);
-					rating.classList.remove("post-rating-selected");
-				}
-			});
+                ratings.forEach(otherRating => {
+                    if (otherRating !== rating && otherRating.classList.contains("post-rating-selected")) {
+                        const otherCount = otherRating.querySelector(".post-rating-count");
+                        otherCount.textContent = Math.max(0, Number(otherCount.textContent) - 1);
+                        otherRating.classList.remove("post-rating-selected");
+                    }
+                });
 
-			rating.classList.add("post-rating-selected");
+                rating.classList.add("post-rating-selected");
 
-			const likeOrDislike = likeRating === rating ? "like" : "dislike";
-			const response = await fetch(`/posts/${postId}/${likeOrDislike}`);
-			const body = await response.json();
-		});
-	});
+                const likeOrDislike = button.textContent === 'thumb_up' ? "like" : "dislike";
+                const response = await fetch(`/posts/${postId}/${likeOrDislike}`);
+                const body = await response.json();
+            });
+        });
+    });
+
+    function animateButton(button) {
+        button.classList.add('clicked');
+        setTimeout(() => {
+            button.classList.remove('clicked');
+        }, 500);
+    }
+
+    function incrementCounter(counterElement) {
+        let count = parseInt(counterElement.textContent);
+        count++;
+        counterElement.textContent = count;
+
+        counterElement.classList.add('bounce');
+        setTimeout(() => {
+            counterElement.classList.remove('bounce');
+        }, 500);
+    }
 });
